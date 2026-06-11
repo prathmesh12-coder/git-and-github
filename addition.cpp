9a89c36a0398ab476c3bf556cd429d12abcc2a19
Prathmesh Sapate <pthsapate12@gmail.com>
2026-06-11 16:58:41 +0530

    added addition file

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a=101;
+    int b=202;
+    int c=a+b;
+
+    std::cout << "Hello, World!" << std::endl;  // basic hello program to test the code run
+    std::cout << "The sum of a and b is: " << c << std::endl;  // output the result of addition
+    return 0;
+}
