471384ea72e245266c7875a69bf6dd091b2a5750
Prathmesh Sapate <pthsapate12@gmail.com>
2026-06-11 17:15:23 +0530

    added comment in the basic hello program

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;  // basic hello program to test the code run
     return 0;
 }
