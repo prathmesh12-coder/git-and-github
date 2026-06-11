4a0b21d5b3e00f9ebbeb16aabc6b4498038f500d
Prathmesh Sapate <pthsapate12@gmail.com>
2026-06-11 17:45:26 +0530

    added substraction file

---
+++ difference.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a=301;
+    int b=201;
+    int c=a-b;
+
+    std::cout << "The difference of a and b is: " << c << std::endl;  // output the result of subtraction
+    return 0;
+}
