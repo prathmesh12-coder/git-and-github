c43c6ac9897e9b94db691aaceefcdef63620e67e
Prathmesh Sapate <pthsapate12@gmail.com>
2026-06-11 17:27:06 +0530

    added multiply file

---
+++ multiplication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a=10;
+    int b=20;
+    int c=a*b;
+
+    std::cout << "The multiplication of a and b is: " << c << std::endl;  // output the result of addition
+    return 0;
+}
